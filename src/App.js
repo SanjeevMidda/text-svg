8c310a18ef3507be16a7e4ce3bb1b49c6854fc51
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-07-16 18:17:57 +0100

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="textContainer">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <text x="10%" y="10%" className="textOne">
+            time
+          </text>
+          <text x="20%" y="20%" className="textTwo">
+            time
+          </text>
+          <text x="30%" y="30%" className="textThree">
+            time
+          </text>
+          <text x="40%" y="40%" className="textFour">
+            time
+          </text>
+          <text x="50%" y="50%" className="textFive">
+            time
+          </text>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default App;
